68aac3413d7b20390f3fd1aabbfa4b9d2a68782c
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-06-11 23:38:31 +0600

    setup eslint

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 import app from "./app";
+import config from "./config";
 
 async function main() {
   try {
     await mongoose.connect(
-      "mongodb+srv://sportFacilities:jFb36r2mOxFMmOzp@cluster0.quaequt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      config.db_url as string
     );
 
     const port = 5000;
 
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
