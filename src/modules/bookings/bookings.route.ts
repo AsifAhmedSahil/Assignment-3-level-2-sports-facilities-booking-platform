2ab8b9c43ff1603bcc639b94c8fd1b76558b7370
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-06-13 00:51:11 +0600

    booking admin and user role token validate

--- src/modules/bookings/bookings.route.ts
+++ src/modules/bookings/bookings.route.ts
@@ -2,11 +2,13 @@ import express  from "express";
 import { bookingControllers } from "./bookings.controller";
 import validationSchema from "../../middlewares/ValidationSchema";
 import { bookingValidations } from "./bookings.validation";
+import { auth } from "../../middlewares/auth";
+import { USER_Role } from "../user/user.contant";
 
 const router = express.Router()
 
-router.post("/",validationSchema(bookingValidations.createBookingValidation),bookingControllers.createBookingController)
-router.get("/",bookingControllers.getAllBookingController)
-router.delete("/:id",bookingControllers.deleteBookingController)
+router.post("/",auth(USER_Role.user),validationSchema(bookingValidations.createBookingValidation),bookingControllers.createBookingController)
+router.get("/",auth(USER_Role.admin),bookingControllers.getAllBookingController)
+router.delete("/:id",auth(USER_Role.user),bookingControllers.deleteBookingController)
 
 export const bookingRoute = router
