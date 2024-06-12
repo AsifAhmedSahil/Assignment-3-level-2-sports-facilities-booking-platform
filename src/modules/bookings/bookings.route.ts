01c8420a3b9c4c884ab6b0d5a21ec93693321b99
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-06-12 13:34:05 +0600

    booking validation setup

--- src/modules/bookings/bookings.route.ts
+++ src/modules/bookings/bookings.route.ts
@@ -1,9 +1,11 @@
 import express  from "express";
 import { bookingControllers } from "./bookings.controller";
+import validationSchema from "../../middlewares/ValidationSchema";
+import { bookingValidations } from "./bookings.validation";
 
 const router = express.Router()
 
-router.post("/",bookingControllers.createBookingController)
+router.post("/",validationSchema(bookingValidations.createBookingValidation),bookingControllers.createBookingController)
 router.get("/",bookingControllers.getAllBookingController)
 router.delete("/:id",bookingControllers.deleteBookingController)
 
