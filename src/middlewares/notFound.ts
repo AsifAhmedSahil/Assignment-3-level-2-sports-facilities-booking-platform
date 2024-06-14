47260570e3a336bbce4284f602c34617b06d939b
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-06-14 21:32:55 +0600

    not found route

--- src/middlewares/notFound.ts
+++ src/middlewares/notFound.ts
@@ -1,14 +1,15 @@
-import  { NextFunction, Request, Response} from 'express';
+
+// import { error } from 'console';
+import  {  NextFunction, Request, Response} from 'express';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, no-unused-vars, @typescript-eslint/no-unused-vars
-const notFound = (err:any,req: Request, res: Response, next:NextFunction) =>{
+const notFound = (req: Request, res: Response,next:NextFunction) =>{
     
   
     return res.status(404).json({
       success:false,
       statusCode:404,
       message: "Api Not Found",
-      error: ""
       
     })
   }
